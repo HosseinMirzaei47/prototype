0fd294f5e4a855df599750af63e64f2182cff7f5
HosseinMirzaei47 <hosseinhidd@gmail.com>
2020-10-23 17:35:50 +0330

    Initial commit.

---
+++ app/src/main/java/com/example/prototype/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.prototype
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.prototype.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_second, R.id.navigation_users
+            )
+        )
+
+        val navController = findNavController(R.id.navHostFragment)
+        setupActionBarWithNavController(
+            navController,
+            appBarConfiguration
+        )
+        navView.setupWithNavController(navController)
+    }
+}
