096bce33ce2e1d178fe72ec8b7a0fa465bcd8394
hosseinmirzazadeh <hosseinhidd@gmail.com>
2020-10-26 15:40:59 +0330

    Register branch initial commit.

---
+++ app/src/main/java/com/example/prototype/features/home/ui/RegisterFragment.kt
@@ -0,0 +1,10 @@
+package com.example.prototype.features.home.ui
+
+import androidx.fragment.app.Fragment
+import com.example.prototype.R
+
+class RegisterFragment : Fragment(R.layout.fragment_register) {
+
+
+
+}
