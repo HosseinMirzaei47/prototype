da1f442d973cec174e98771d8e8e424062f667c6
hosseinmirzazadeh <hosseinhidd@gmail.com>
2020-10-28 12:52:19 +0330

    UI improvement.

--- app/src/main/java/com/example/prototype/MainActivity.kt
+++ app/src/main/java/com/example/prototype/MainActivity.kt
@@ -5,19 +5,41 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.LiveData
 import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.prototype.R
+import com.example.prototype.core.storage.data.Settings
 import com.example.prototype.databinding.ActivityMainBinding
 import com.example.prototype.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var sharedPreference: Settings
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        if (!sharedPreference.authToken.isNullOrEmpty()) {
+            changeGraph()
+        }
+
+    }
+
+    private fun changeGraph() {
+        val navHostFragment = mainNavHost as NavHostFragment
+        val inflater = navHostFragment.navController.navInflater
+        val graph = inflater.inflate(R.navigation.auth_graph)
+        graph.startDestination = R.id.navigation_dashboard
+        navHostFragment.navController.graph = graph
     }
 
 }
