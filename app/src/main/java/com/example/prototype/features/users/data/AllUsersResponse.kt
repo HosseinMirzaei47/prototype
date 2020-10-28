b00a597f774aa6baabf1b96bc1df949285a5c9c0
hosseinmirzazadeh <hosseinhidd@gmail.com>
2020-10-28 12:15:17 +0330

    Moshi implementation.

--- app/src/main/java/com/example/prototype/features/users/data/AllUsersResponse.kt
+++ app/src/main/java/com/example/prototype/features/users/data/AllUsersResponse.kt
@@ -1,11 +1,10 @@
 package com.example.prototype.features.users.data
 
-import com.google.gson.annotations.Expose
 import com.squareup.moshi.Json
 
 data class AllUsersResponse(
     val ad: Ad,
-    @Json(name = "data")
+    @field:Json(name = "data")
     val users: List<User>,
     val page: Int,
     val per_page: Int,
