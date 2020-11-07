8cf48c5db03012138a0e19f4acae92cfabbccc7f
hosseinmirzazadeh <hosseinhidd@gmail.com>
2020-11-07 14:23:56 +0330

    Hid status bar.

--- app/src/main/java/com/example/prototype/features/users/data/UsersPagingSource.kt
+++ app/src/main/java/com/example/prototype/features/users/data/UsersPagingSource.kt
@@ -4,13 +4,11 @@ import androidx.paging.PagingSource
 import retrofit2.HttpException
 import javax.inject.Inject
 
-private const val USERS_STARTING_PAGE_INDEX = 1
-
 class UsersPagingSource @Inject constructor(
     private val usersRemoteDataSource: UsersRemoteDataSource
 ) : PagingSource<Int, User>() {
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, User> {
-        val position = params.key ?: USERS_STARTING_PAGE_INDEX
+        val position = params.key ?: 1
 
         return try {
             val response = usersRemoteDataSource.getUsers(position.toString())
@@ -18,7 +16,7 @@ class UsersPagingSource @Inject constructor(
 
             LoadResult.Page(
                 data = if (!users.isNullOrEmpty()) users else listOf(),
-                prevKey = if (position == USERS_STARTING_PAGE_INDEX) null else position - 1,
+                prevKey = if (position == 1) null else position - 1,
                 nextKey = if (users?.isEmpty()!!) null else position + 1
             )
         } catch (e: HttpException) {
